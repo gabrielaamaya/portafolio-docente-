f04442a8f5172509bbf2d86b3e3a9ef9a574bf9a
gabrielaamaya <amayagabriela073@gmail.com>
2026-04-29 14:44:54 -0600

    reacion de los archivos

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", function(){
+
+let header = document.querySelector("header");
+
+if(window.scrollY > 50){
+header.style.boxShadow = "0 5px 15px rgba(0,0,0,0.2)";
+}else{
+header.style.boxShadow = "none";
+}
+
+});
